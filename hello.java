abd50c079ab5a2dd412f751d1e924752a58f8329
zckixw11 <zckixw11@gmail.com>
2022-02-18 11:56:30 +0900

    test1

---
+++ hello.java
@@ -0,0 +1,31 @@
+class Hello {
+
+    private String say;
+    private String age;
+
+    public String getSay() {
+        return say;
+    }
+
+    public void setSay(String say) {
+        this.say = say;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+
+
+    public static void main(String[] args){
+        Hello hello = new Hello();
+
+        hello.setSay("leedongwook");
+
+        System.out.println(hello.say);
+    }
+}
